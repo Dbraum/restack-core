81a9a054cf51912780d41cb448ee50bb4b620061
MAC_CN <757246444@qq.com>
2016-10-28 17:05:01 +0800

    增加获取saga方法

--- src/app.js
+++ src/app.js
@@ -1,89 +1,137 @@
 // core frameworks
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
 
 import configureStore from './store/configureStore';
-import createReducer from './utils/createReducer';
 import i18n from './i18n';
 
 import modal from './reducers/modal'
+import invariant from 'invariant';
+import createReducer from 'redux-updeep';
+import createSaga from './utils/createSaga';
 
 export default class App {
 
-  constructor(config) {
-    this.config = config || {}
-    this._model = []
-  }
+	constructor(config) {
+		this.config = config || {}
+		this._model = []
+	}
 
-  set routes(routes) {
-    this._routes = routes
-  }
+	set routes(routes) {
+		this._routes = routes
+	}
 
-  set reducers(reducers) {
-    this._reducers = reducers
-  }
+	set reducers(reducers) {
+		this._reducers = reducers
+	}
 
-  set middlewares(middlewares) {
-    this._middlewares = middlewares
-  }
-  set model(model) {
-    this._model = model
-  }
+	set middlewares(middlewares) {
+		this._middlewares = middlewares
+	}
 
-  fetchLocalePromise(needsI18n, userLocale, defaultLocale) {
-    if (needsI18n && userLocale != defaultLocale) {
-      return i18n.fetchLocaleData(userLocale)
-      .then( localeData => {
-        return {locale: userLocale, localeData}
-      } )
-    } else {
-      return Promise.resolve({locale: userLocale, localeData: {}})
-    }
-  }
+	set model(model) {
+		this._model = model.map(checkModel)
+	}
 
-  createRootComponent({locale, localeData}) {
+	fetchLocalePromise(needsI18n, userLocale, defaultLocale) {
+		if (needsI18n && userLocale != defaultLocale) {
+			return i18n.fetchLocaleData(userLocale)
+				.then(localeData => {
+					return {locale: userLocale, localeData}
+				})
+		} else {
+			return Promise.resolve({locale: userLocale, localeData: {}})
+		}
+	}
 
-    let reducers = {
-      modal,
-      ...this._reducers
-    }
-    this._model.reduce(function (prev,curr) {
-      const {namespace,state,reducer} = curr;
-      prev[namespace]= createReducer(state,reducer)
-    },reducers)
+	replaceReducer(nextReducers, models) {
+		let reducers = {
+			modal,
+			...nextReducers
+		}, sagas = [];
+		for (let m of models) {
+			const {namespace, state, effects} = m;
+			reducers[namespace] = createReducer(namespace, state);
+			if (effects) sagas.push(createSaga(m.effects, m));
+		}
+		this.store.hotReplaceReducer(reducers)
+	}
 
-    const initialState = window.__INITIAL_STATE__ || {};
-    const store = configureStore(reducers, initialState, this._middlewares)
-    const history = syncHistoryWithStore(browserHistory, store)
-    const i18nTools = new i18n.Tools({localeData, locale});
+	createRootComponent({locale, localeData}) {
 
-    return (
-      <Provider store={store}>
-        <i18n.Provider i18n={i18nTools}>
-          <Router history={history} children={this._routes} />
-        </i18n.Provider>
-      </Provider>
-    )
-  }
+		let reducers = {
+			modal,
+			...this._reducers
+		}
+		let sagas = []
 
-  async render(el) {
 
-    const { locales, defaultLocale } = this.config;
+		for (let m of this._model) {
+			const {namespace, state, effects} = m;
+			reducers[namespace] = createReducer(namespace, state);
+			if (effects) sagas.push(createSaga(m.effects, m));
+		}
 
-    const userLocale = i18n.getUserLocale(defaultLocale);
 
-    const {locale, localeData} = await this.fetchLocalePromise(locales != null, userLocale, defaultLocale)
+		// create store
+		const initialState = window.__INITIAL_STATE__ || {};
+		const store = configureStore(reducers, initialState, this._middlewares)
 
-    const RootComponent = this.createRootComponent({locale, localeData})
+		// start saga
+		sagas.forEach(store.run);
 
-    if (el) {
-      ReactDOM.render(RootComponent, el)
-    } else {
-      return RootComponent;
-    }
+		this.store = store;
 
-  }
+
+		const history = syncHistoryWithStore(browserHistory, store)
+		const i18nTools = new i18n.Tools({localeData, locale});
+
+		return (
+			<Provider store={store}>
+				<i18n.Provider i18n={i18nTools}>
+					<Router history={history} children={this._routes}/>
+				</i18n.Provider>
+			</Provider>
+		)
+	}
+
+	async render(el) {
+
+		const {locales, defaultLocale} = this.config;
+
+		const userLocale = i18n.getUserLocale(defaultLocale);
+
+		const {locale, localeData} = await this.fetchLocalePromise(locales != null, userLocale, defaultLocale)
+
+		const RootComponent = this.createRootComponent({locale, localeData})
+
+		if (el) {
+			ReactDOM.render(RootComponent, el)
+		} else {
+			return RootComponent;
+		}
+
+	}
+}
+
+
+function checkModel(m) {
+	// Clone model to avoid prefixing namespace multiple times
+	const model = {...m};
+	const {namespace} = model;
+
+	invariant(
+		namespace,
+		'app.model: namespace should be defined'
+	);
+	invariant(
+		namespace !== 'routing',
+		'app.model: namespace should not be routing, it\'s used by react-redux-router'
+	);
+
+
+	return model;
 }
---
+++ src/utils/createSaga.js
@@ -0,0 +1,88 @@
+import invariant from 'invariant';
+import warning from 'warning';
+import * as sagaEffects from 'redux-saga/effects';
+import { takeEvery, takeLatest } from 'redux-saga/lib/internal/sagaHelpers';
+
+const SEP = '/';
+
+
+export default function createSaga(effects, model) {
+	return function *() {
+		for (let key in effects) {
+			const watcher = getWatcher(key, effects[key], model);
+			yield sagaEffects.fork(watcher);
+		}
+	}
+}
+
+
+function getWatcher(key,_effect,model) {
+	let effect = _effect;
+	let type = 'takeEvery';
+	let ms;
+	if (Array.isArray(_effect)) {
+		effect = _effect[0];
+		const opts = _effect[1];
+		if (opts && opts.type) {
+			type = opts.type;
+			if (type === 'throttle') {
+				invariant(
+					opts.ms,
+					'app.start: opts.ms should be defined if type is throttle'
+				);
+				ms = opts.ms;
+			}
+		}
+		invariant(
+			['watcher', 'takeEvery', 'takeLatest', 'throttle'].indexOf(type) > -1,
+			'app.start: effect type should be takeEvery, takeLatest, throttle or watcher'
+		);
+	}
+
+	function *sagaWithCatch(...args) {
+		/*try {
+			yield effect(...args.concat(createEffects(model)));
+		} catch(e) {
+			onError(e);
+		}*/
+
+		yield effect(...args.concat(createEffects(model)));
+	}
+
+	switch (type) {
+		case 'watcher':
+			return sagaWithCatch;
+		case 'takeLatest':
+			return function*() {
+				yield takeLatest(key, sagaWithCatch);
+			};
+		// takeEvery
+		default:
+			return function*() {
+				yield takeEvery(key, sagaWithCatch);
+			};
+	}
+}
+
+
+function prefixType(type, model) {
+	/*const prefixedType = `${model.namespace}${SEP}${type}`;
+	if ((model.reducers && model.reducers[prefixedType])
+		|| (model.effects && model.effects[prefixedType])) {
+		return prefixedType;
+	}*/
+	return type;
+}
+
+function createEffects(model) {
+	function put(action) {
+		const { type } = action;
+		invariant(type, 'dispatch: action should be a plain Object with type');
+		/*warning(
+			type.indexOf(`${model.namespace}${SEP}`) !== 0,
+			`effects.put: ${type} should not be prefixed with namespace ${model.namespace}`
+		);*/
+		return sagaEffects.put({ ...action, type: prefixType(type, model) });
+	}
+	return { ...sagaEffects, put };
+}
