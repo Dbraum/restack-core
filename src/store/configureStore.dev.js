bcaa6a17fa83bec9fd3e053c6f014ca40cea6e9c
MAC_CN <757246444@qq.com>
2016-10-28 17:04:23 +0800

    dev模式增加热加载

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,40 +1,54 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
 // middlewares
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import DevTools from '../containers/DevTools'
 // reducers
-import { routerReducer, routerMiddleware as createRouterMiddleware } from 'react-router-redux'
-import { browserHistory } from 'react-router'
+import {routerReducer, routerMiddleware as createRouterMiddleware} from 'react-router-redux'
+import {browserHistory} from 'react-router'
+
+import createSagaMiddleware from 'redux-saga/lib/internal/middleware';
 
 export default function configureStore(rootReducer, initialState, middlewares = []) {
 
-  rootReducer = combineReducers({
-    ...rootReducer,
-    routing: routerReducer
-  })
+	rootReducer = combineReducers({
+		...rootReducer,
+		routing: routerReducer
+	})
 
-  // enable navigation via redux actions
-  // https://github.com/reactjs/react-router-redux#what-if-i-want-to-issue-navigation-events-via-redux-actions
-  // http://stackoverflow.com/questions/32612418/transition-to-another-route-on-successful-async-redux-action/32922381#32922381
-  const routerMiddleware = createRouterMiddleware(browserHistory)
+	// enable navigation via redux actions
+	// https://github.com/reactjs/react-router-redux#what-if-i-want-to-issue-navigation-events-via-redux-actions
+	// http://stackoverflow.com/questions/32612418/transition-to-another-route-on-successful-async-redux-action/32922381#32922381
+	const routerMiddleware = createRouterMiddleware(browserHistory)
+	//apply saga middleware
+	const sagaMiddleware = createSagaMiddleware();
+	const store = createStore(
+		rootReducer,
+		initialState,
+		compose(
+			applyMiddleware(thunk, createLogger(), routerMiddleware, sagaMiddleware, ...middlewares),
+			DevTools.instrument()
+		)
+	)
 
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(thunk, createLogger(), routerMiddleware, ...middlewares),
-      DevTools.instrument()
-    )
-  )
+	// extend store
+	store.run = sagaMiddleware.run ;
 
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('../reducers', () => {
-  //     const nextRootReducer = require('../reducers').default
-  //     store.replaceReducer(nextRootReducer)
-  //   })
-  // }
+	store.hotReplaceReducer = (reducer) => {
+		store.replaceReducer(combineReducers({
+			...reducer,
+			routing: routerReducer
+		}))
+	}
 
-  return store
+	// if (module.hot) {
+	//   // Enable Webpack hot module replacement for reducers
+	//   module.hot.accept('../reducers', () => {
+	//     const nextRootReducer = require('../reducers').default
+	//     store.replaceReducer(nextRootReducer)
+	//   })
+	// }
+
+
+	return store
 }
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,22 +1,29 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
 // middlewares
 import thunk from 'redux-thunk'
 // reducers
-import { routerReducer, routerMiddleware as createRouterMiddleware } from 'react-router-redux'
-import { browserHistory } from 'react-router'
+import {routerReducer, routerMiddleware as createRouterMiddleware} from 'react-router-redux'
+import {browserHistory} from 'react-router'
+import createSagaMiddleware from 'redux-saga/lib/internal/middleware';
 
 export default function configureStore(rootReducer, initialState, middlewares = []) {
 
-  rootReducer = combineReducers({
-    ...rootReducer,
-    routing: routerReducer
-  })
+	rootReducer = combineReducers({
+		...rootReducer,
+		routing: routerReducer
+	})
 
-  const routerMiddleware = createRouterMiddleware(browserHistory)
+	const routerMiddleware = createRouterMiddleware(browserHistory)
+	//apply saga middleware
+	const sagaMiddleware = createSagaMiddleware();
 
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, routerMiddleware, ...middlewares)
-  )
+	const store = createStore(
+		rootReducer,
+		initialState,
+		applyMiddleware(thunk, routerMiddleware, sagaMiddleware, ...middlewares)
+	)
+	// extend store
+	store.run = sagaMiddleware.run ;
+
+	return store
 }
